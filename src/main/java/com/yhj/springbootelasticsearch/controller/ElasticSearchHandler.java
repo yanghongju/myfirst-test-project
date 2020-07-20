b16933be1862165a489ca02920e722ed49177dbc
yanghongju <yang.hongju@sinoheli.cn>
2020-07-20 10:52:42 +0800

    hehe

--- src/main/java/com/yhj/springbootelasticsearch/controller/ElasticSearchHandler.java
+++ src/main/java/com/yhj/springbootelasticsearch/controller/ElasticSearchHandler.java
@@ -6,5 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ElasticSearchHandler {
 
 
+    public static void main(String[] args) {
+        System.err.println("test git reset head");
+    }
+
 
 }
